0eb38be48e34b11806b3be8ea0303dbc3ddd0820
Alvin Yap <alyap@u91z05s03.42kl.edu.my>
2023-10-02 21:53:22 +0800

    chore: ex00 docs

--- ex00/give_bmi.py
+++ ex00/give_bmi.py
@@ -2,10 +2,17 @@ def give_bmi(
         height: list[int or float],
         weight: list[int or float]
 ) -> list[int or float]:
-    '''
-    Takes in 2 arrays, one representing height and the other representing
-    weight, and return an array of bmi numbers
-    '''
+    """
+    Takes in a list of height and a list of weight, and return
+    the BMI value.
+
+    Parameters:
+    param1 (height: list[int or float]): List of height.
+    param2 (weight: list[int or float]): List of weight.
+
+    Returns:
+    list[int or float]: A list of either int of float as BMI value.
+    """
     assert len(height) == len(weight), "invalid heght and weight length"
     for h, w in zip(height, weight):
         if not isinstance(h, (int, float)) or not isinstance(w, (int, float)):
@@ -15,8 +22,15 @@ def give_bmi(
 
 def apply_limit(bmi: list[int or float], limit: int) -> list[bool]:
     '''
-    Takes in an array of int or float and an int limit as argument,
-    and return a array of boolean on whether the bmi has exceeded
+    Takes in a list of BMI value and a limit, and return a list
+    of booleans on whether the BMI value exceeded the limit.
+
+    Parameters:
+    param1 (bmi: list[int or float]): List of BMI value.
+    param2 (limit: int): Limit value.
+
+    Returns:
+    list[bool]: A boolean on whether the input BMI value exceeded
     limit.
     '''
     return [b > limit for b in bmi]
