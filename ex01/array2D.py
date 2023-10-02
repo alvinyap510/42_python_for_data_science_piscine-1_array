819f3ee9b972e8fed3bdc5f87c97d12a7f96ff11
Alvin Yap <alyap@u91z05s03.42kl.edu.my>
2023-10-02 21:54:38 +0800

    refactor: use numpy

--- ex01/array2D.py
+++ ex01/array2D.py
@@ -1,10 +1,22 @@
+import numpy as np
+
+
 def slice_me(family: list, start: int, end: int) -> list:
-    assert isinstance(family, list), "arguments are bad"
+    '''
+    Takes in a list and return a truncated list.
+    Parameters:
+    param1 (family: list): A list that will be truncated.
+    param2 (start: int): Starting index.
+    param3 (end: int): Ending index.
+    Returns:
+    list: The list that has been truncated.
+    '''
+    assert isinstance(family, list), "first param must be a list is"
     for f in family:
-        child_length = len(family[0])
-        assert isinstance(f, list), "arguments are bad"
-        assert len(f) == child_length, "arguments are bad"
-    print(f"My shape is : ({len(family)}, {child_length})")
+        dimension = len(family[0])
+        assert isinstance(f, list), "non-list child detected"
+        assert len(f) == dimension, "all childs must have same dimension"
+    print(f"My shape is : {np.array(family).shape}")
     new_family = family[start:end]
-    print(f"My new shape is : ({len(new_family)}, {child_length})")
+    print(f"My new shape is : ({np.array(new_family).shape})")
     return new_family
