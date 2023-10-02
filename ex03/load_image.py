8a7fae4f83a79c46e14a9abae186928778ecbfa1
Alvin Yap <alyap@u91z05s03.42kl.edu.my>
2023-10-02 23:07:13 +0800

    chore: move computer

--- ex03/load_image.py
+++ ex03/load_image.py
@@ -2,12 +2,24 @@ from PIL import Image
 import numpy as np
 
 
-def ft_load(path: str):
-    img = Image.open(path)
-    width, height = img.size
-    if img.mode != "RGB":
-        img = img.convert("RGB")
-    img_array = np.array(img)
-    print(
-        f"The shape of image is: ({height}, {width}, {len(img_array[0][0])})")
-    return img_array
+def ft_load(path: str) -> np.ndarray:
+    '''
+    Load an image, prints out its size, and convert it
+    into numpy array.
+
+    Parameters:
+    param1 (path: str): Path to the file to be converted.
+
+    Returns:
+    np.ndarray: Numpy array that represents the image
+    '''
+    try:
+        img = Image.open(path)
+        if img.mode != "RGB":
+            img = img.convert("RGB")
+        img_array = np.array(img)
+        print(
+            f"The shape of image is: {img_array.shape}")
+        return img_array
+    except Exception as e:
+        print(f"Error: {e}")
--- ex03/zoom.py
+++ ex03/zoom.py
@@ -1,27 +1,58 @@
-from PIL import Image
 import numpy as np
+from PIL import Image
 from load_image import ft_load
+import matplotlib.pyplot as plt
 
 
 def main():
-    img_array = ft_load("animal.jpeg")
-    print(img_array)
-    img_array = img_array[70:470]
-    new_img_array = []
-    for i in img_array:
-        new_img_array.append(i[500:900])
-    new_img_array = np.array(new_img_array, dtype=np.uint8)
-    new_img = Image.fromarray(new_img_array)
-    if (new_img.mode != "L"):
-        new_img = new_img.convert("L")
-    print(new_img)
-    new_array = np.array(new_img)
-    print(new_array)
-    three_d_array = np.array([[[element] for element in row]
-                             for row in new_array])
-    print(three_d_array)
+    try:
+        img_array = ft_load("animal.jpeg")
+        print(img_array)
 
-    new_img.save("processed_animal.jpeg")
+        start_height = 70
+        end_height = 470
+        start_column = 450
+        end_column = 850
+
+        # new_img_array = img_array[70:470, 500:900, 2]
+        # # new_img = Image.fromarray(new_img_array)
+        # # new_img.save("processed_animal.jpeg")
+        # plt.imshow(new_img_array, cmap="gray")
+        # plt.show()
+
+        new_img_array = img_array[70:470, 500:900, :]
+        new_img = Image.fromarray(new_img_array)
+        if (new_img.mode != "L"):
+            new_img = new_img.convert("L")
+        new_img.save("processed_animal.jpeg")
+        plt.imshow(new_img)
+        plt.show()
+
+
+
+
+
+
+    # img_array = img_array[70:470]
+    # new_img_array = []
+    # for i in img_array:
+    #     new_img_array.append(i[500:900])
+    # new_img_array = np.array(new_img_array, dtype=np.uint8)
+    # new_img = Image.fromarray(new_img_array)
+    # if (new_img.mode != "L"):
+    #     new_img = new_img.convert("L")
+    # print(new_img)
+    # new_array = np.array(new_img)
+    # print(new_array)
+    # three_d_array = np.array([[[element] for element in row]
+    #                          for row in new_array])
+    # print(three_d_array)
+
+    # new_img.save("processed_animal.jpeg")
+    except Exception as e:
+        print(f"Error: {e}")
+    except KeyboardInterrupt:
+        print("K thanks bye")
 
 
 if __name__ == "__main__":
