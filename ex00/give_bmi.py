b9087ba58f6c56d6d5b098579cc462a6ed2fc428
Alvin Yap <alyap@u91z05s03.42kl.edu.my>
2023-10-02 21:08:35 +0800

    fix: ex00

--- ex00/give_bmi.py
+++ ex00/give_bmi.py
@@ -1,11 +1,19 @@
 def give_bmi(height: list[int or float], weight: list[int or float]) -> list[int or float]:
-    if len(height) != len(weight):
-        raise AssertionError("the arguments are bad")
+    '''
+    Takes in 2 arrays, one representing height and the other representing weight,
+    and return an array of bmi numbers
+    '''
+    assert len(height) == len(weight), "the length of height array and weight array should be the same"
     for h, w in zip(height, weight):
         if not isinstance(h, (int, float)) or not isinstance(w, (int, float)):
-            raise AssertionError("the arguments are bad")
+            raise AssertionError("arguments must be type int or float")
     return [w / (h ** 2) for h, w in zip(height, weight)]
 
 
 def apply_limit(bmi: list[int or float], limit: int) -> list[bool]:
+    '''
+    Takes in an array of int or float and an int limit as argument,
+    and return a array of boolean on whether the bmi has exceeded
+    limit.
+    '''
     return [b > limit for b in bmi]
