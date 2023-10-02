99314879ca24542ace0d798bb6bddbc96cfc11a1
Alvin Yap <leaf.alvin@gmail.com>
2023-10-03 07:24:06 +0800

    feat: complete ex03 using matplotlib

--- ex03/zoom.py
+++ ex03/zoom.py
@@ -1,10 +1,19 @@
-import numpy as np
 from PIL import Image
 from load_image import ft_load
 import matplotlib.pyplot as plt
 
 
 def main():
+    '''
+    Load an image, prints out its size, zoom in the image,
+    converts it to grayscale and use matplotlib to plot it.
+
+    Parameters:
+
+    Returns:
+
+    '''
+
     try:
         img_array = ft_load("animal.jpeg")
         print(img_array)
@@ -14,41 +23,12 @@ def main():
         start_column = 450
         end_column = 850
 
-        # new_img_array = img_array[70:470, 500:900, 2]
-        # # new_img = Image.fromarray(new_img_array)
-        # # new_img.save("processed_animal.jpeg")
-        # plt.imshow(new_img_array, cmap="gray")
-        # plt.show()
-
-        new_img_array = img_array[70:470, 500:900, :]
+        new_img_array = img_array[start_height:end_height,
+                                  start_column:end_column, :]
         new_img = Image.fromarray(new_img_array)
-        if (new_img.mode != "L"):
-            new_img = new_img.convert("L")
-        new_img.save("processed_animal.jpeg")
-        plt.imshow(new_img)
+        new_img = new_img.convert("L")
+        plt.imshow(new_img, cmap="gray")
         plt.show()
-
-
-
-
-
-
-    # img_array = img_array[70:470]
-    # new_img_array = []
-    # for i in img_array:
-    #     new_img_array.append(i[500:900])
-    # new_img_array = np.array(new_img_array, dtype=np.uint8)
-    # new_img = Image.fromarray(new_img_array)
-    # if (new_img.mode != "L"):
-    #     new_img = new_img.convert("L")
-    # print(new_img)
-    # new_array = np.array(new_img)
-    # print(new_array)
-    # three_d_array = np.array([[[element] for element in row]
-    #                          for row in new_array])
-    # print(three_d_array)
-
-    # new_img.save("processed_animal.jpeg")
     except Exception as e:
         print(f"Error: {e}")
     except KeyboardInterrupt:
